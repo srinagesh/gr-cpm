c7481eaf3aae4f913da7e19b2b0c3df902eab3a0
Srinagesh <srinageshsharma@gmail.com>
2012-05-17 08:31:00 -0400

    commit 017

--- gr-howto-write-a-block-cmake/lib/howto_phase_correction_nda_vcvc.cc
+++ gr-howto-write-a-block-cmake/lib/howto_phase_correction_nda_vcvc.cc
@@ -13,6 +13,7 @@
 #include <stdexcept>
 #include <stdlib.h>
 #include <gr_sincos.h>
+#include <gr_expj.h>
 #include <math.h>
 #include <gr_math.h>
 #include <digital_constellation.h>
@@ -43,31 +44,15 @@ howto_phase_correction_nda_vcvc::howto_phase_correction_nda_vcvc(unsigned int lv
 		z[j]=gr_complex(y[j].real(),0);
 		cout<<"Z is "<<z[j]<<endl;
 	}
-//	cout<<"c_real "<<z[0].real()<<" c_imag "<<z[0].imag()<<endl;
 	cout<<"this program has passed through the pc constructor"<<endl;
 }
 
-//howto_phase_correction_vcvc::~howto_phase_correction_vcvc()
-//{
-//	delete digital_constellation_sptr d_cnst;
-//}
 
-float howto_phase_correction_nda_vcvc::mod_2pi(float in_ph)
+float howto_phase_correction_nda_vcvc::phase_detector(gr_complex sample, gr_complex ref)
 {
-	if (in_ph>M_PI)
-		return in_ph-M_PI;
-	else if (in_ph<-M_PI)
-		return in_ph+M_PI;
-	else
-		return in_ph;
-}
-
-float howto_phase_correction_nda_vcvc::phase_detector(gr_complex sample, float ref_phase)
-{
-	float sample_phase;
-	sample_phase = gr_fast_atan2f(sample.imag(),sample.real());
-	//cout<<sample_phase*180/M_PI<<endl;
-	return mod_2pi(sample_phase-ref_phase);
+	float phase_diff;
+	phase_diff = sample.real()*ref.imag() + sample.imag()*ref.real();
+	return phase_diff;
 }
 
 int howto_phase_correction_nda_vcvc::work(int noutput_items,
@@ -77,13 +62,12 @@ int howto_phase_correction_nda_vcvc::work(int noutput_items,
 	const gr_complex *in = (const gr_complex*)input_items[0];
 	gr_complex *out = (gr_complex*)output_items[0];
 	float t_real,t_imag, error, temp_real, temp_imag, d_phase_parameter;
-	gr_complex t_complex, pll_ip, rec_sig_sum;
+	gr_complex t_complex, pll_ip, rec_sig_sum, refph;
 	std::vector<gr_complex> rec_sig;
 
 	rec_sig.resize(d_lvec);
 
 	for(unsigned int n=0;n<noutput_items;n++){
-	//	cout<<"first line"<<endl;
 
 //raise input signal to the nth power------------------------------------
 		for(unsigned int i=0;i<d_power;i++){
@@ -96,44 +80,34 @@ int howto_phase_correction_nda_vcvc::work(int noutput_items,
 				rec_sig[j]=gr_complex(temp_real,temp_imag);
 			}
 		}
-//		cout<<"c_real "<<rec_sig[0].real()<<" c_imag "<<rec_sig[0].imag()<<endl;
-//-----------------------------------------------------------------------	
+
+//-----------------------------------------------------------------------
+	
 		rec_sig_sum=gr_complex(0,0);
-		for(unsigned int i=0;i<d_lvec;i++){				//!!NOTE:change this back to d_lvec
+		for(unsigned int i=0;i<d_lvec;i++){	
 			temp_real=(rec_sig[i].real() * z[i].real() + rec_sig[i].imag() * z[i].imag())/pow(z[i].real(),2);
 			temp_imag=(-rec_sig[i].real() * z[i].imag() + rec_sig[i].imag() * z[i].real())/pow(z[i].real(),2);
 			temp_real=rec_sig_sum.real()+temp_real;
 			temp_imag=rec_sig_sum.imag()+temp_imag;
 			rec_sig_sum=gr_complex(temp_real,temp_imag);
-		}
-	//	cout<<"c_real "<<rec_sig_sum.real()<<" c_imag "<<rec_sig_sum.imag()<<endl;	
-	
-		//rec_sig_sum is the input to the phase locked loop.
+		}	
 
-		
 		d_phase_parameter=(float)d_phase/d_power;
 		gr_sincosf(d_phase_parameter,&t_imag,&t_real);
 		t_complex = gr_complex(t_real,-t_imag);
-	//	cout<<"second line"<<endl;
 
 		for(unsigned int i=0;i<d_lvec;i++){
-	//		cout<<"loop"<< i <<endl;
 			temp_real=in[n*d_lvec+i].real() * t_complex.real() - in[n*d_lvec+i].imag() * t_complex.imag();
 			temp_imag=in[n*d_lvec+i].real() * t_complex.imag() + in[n*d_lvec+i].imag() * t_complex.real();
 			out[n*d_lvec+i]=gr_complex(temp_real,temp_imag);
 		}
 		
-	//	cout<<"d_lvec="<<d_lvec<<endl;
-		
-		error = phase_detector(rec_sig_sum,d_phase);
+		refph = gr_expj(-d_phase);
+		error = phase_detector(rec_sig_sum, refph);
 		advance_loop(error);
-	//	cout<<"d_phase "<<d_phase<<endl;
-	//	cout<<"d_freq "<<d_freq<<endl;
 		phase_wrap();
 		frequency_limit();
-	//	cout<<"end of loop"<<endl;
 	}
-	//cout<<error<<endl;
 		
 return noutput_items;
 }
